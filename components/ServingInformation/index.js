2503723bb94b1a6f8d9121516b9fa1fba92f76a5
cgnicolas <33070060+cgnicolas@users.noreply.github.com>
2024-07-25 23:15:34 -0400

    Fix Serving Information

--- components/ServingInformation/index.js
+++ components/ServingInformation/index.js
@@ -5,6 +5,7 @@ import { useUser } from '@auth0/nextjs-auth0/client';
 import React, { useContext, useEffect } from 'react';
 
 export default function ServingInformation() {
+  const { serving } = useContext(DoritoContext);
   return (
     serving && (
       <div className="text-lg font-semibold">
