9a2fba5675eb14aa472c97c7c950a0358ee9d4f5
cgnicolas <33070060+cgnicolas@users.noreply.github.com>
2024-07-25 22:27:00 -0400

    Use Tabs in CCPage

--- components/pages/CCPage/index.js
+++ components/pages/CCPage/index.js
@@ -1,13 +1,26 @@
 'use client';
+import ServingChart from '@/components/ServingChart';
+import { Tabs, TabsContent, TabsList, TabsTrigger } from '@/components/ui/tabs';
 import Uploader from '@/components/Uploader/Uploader';
 import { DoritoProvider } from '@/context/DoritoContext';
-import React, { useContext } from 'react';
+import React from 'react';
 
 export default function CCPage() {
   return (
     <DoritoProvider>
       <div className="px-4 sm:px-8 md:px-16 lg:px-32 flex w-full h-screen flex-col items-center justify-center">
-        <Uploader />
+        <Tabs defaultValue="account" className="w-auto">
+          <TabsList className="grid w-full grid-cols-2">
+            <TabsTrigger value="upload">Upload</TabsTrigger>
+            <TabsTrigger value="chart">Chart</TabsTrigger>
+          </TabsList>
+          <TabsContent value="upload">
+            <Uploader />
+          </TabsContent>
+          <TabsContent value="chart">
+            <ServingChart />
+          </TabsContent>
+        </Tabs>
       </div>
     </DoritoProvider>
   );
